508328f4009a73cfa38ccf291a8a87caa5751766
SUJAY  ADKESAR <95465072+sujayadkesar@users.noreply.github.com>
2022-11-09 18:57:36 +0530

    updated with remote host

---
+++ Server.py
@@ -0,0 +1,67 @@
+import socket
+import threading
+
+
+# connection data
+hostname = socket.gethostname()
+get_host_ip = socket.gethostbyname(hostname)
+
+print(f"[*] Connection id : {get_host_ip}")
+
+host = get_host_ip
+port = 45454
+
+# starting server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+# Lists for clients and theri nicknames
+clients = []
+nicknames = []
+
+
+# sending messages to all connected clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+# Handling messages from clients
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        # Accept connection
+
+        client, address = server.accept()
+        print(f"[*] Connection established with {address}")
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+receive()
---
+++ client.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+
+nickname = input("Choose your nickname: ")
+
+
+
+get_connection_ip = input("Enter the target server ip : ")
+connection_ip = get_connection_ip
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((f'{connection_ip}', 45454))
+
+
+# listening to server and sending nicknames
+
+def receive():
+    while True:
+        try:
+            # Receive message from server
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            # close connection because of error
+            print("An error occured !! ")
+            client.close()
+            break
+
+# sending messages to the server
+def write():
+    while True:
+        message = '{}:{}'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
+
+
+# Starting threads for listening and writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
